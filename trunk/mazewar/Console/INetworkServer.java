f2602d2a16811ad4ede8837722c977795f0378ce
michael.ritche <michael.ritche@f2dbe62d-b44a-0410-9e99-ef0b274e9c5f>
2008-04-13 21:33:51 +0000

    Old stuff.

--- trunk/mazewar/Console/INetworkServer.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public interface INetworkServer
-{
-    public abstract void setPeerListener (INetworkPeer listener);
-
-    void broadcastMessage (Object data)
-        throws IOException, ClassNotFoundException;
-}
